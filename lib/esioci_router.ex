4a06ddbd6f3f002e6f4212c80cfe2f69d2585be2
Grzegorz Eliszewski <grzegorz@eliszewski.pl>
2016-07-16 08:35:21 +0200

    Add support for projects

--- lib/esioci_router.ex
+++ lib/esioci_router.ex
@@ -3,6 +3,7 @@ defmodule EsioCi.Router do
   use Plug.ErrorHandler
   import Plug.Conn
   import Plug.Conn.Utils
+  import Ecto.Query, only: [from: 2]
   require Logger
 
   plug Plug.Logger
@@ -24,37 +25,37 @@ defmodule EsioCi.Router do
   end
 
   # Run build for project
-  get "/api/v1/:project/bld" do
+  post "/api/v1/:project/bld" do
     # Insert build to database
-    Logger.info project
-    #build = %EsioCi.Build{state: "CREATED"}
-    #created_build = EsioCi.Repo.insert!(build)
-    #build_id = created_build.id
+    q = from p in "projects",
+      where: p.name == ^project,
+      select: p.id
 
-    #pid = spawn(EsioCi.Builder, :build, [])
-    #send pid, {self, conn, build_id}
+    p_id = EsioCi.Repo.all(q)
+    case Enum.count(p_id) do
+       0 -> conn
+            |> send_resp(404, "404: Project #{project} not found.")
 
-    conn
-    |> send_resp(200, "Build created")
-  end
+       1 -> build_id = add_build_to_db(List.first(p_id))
+            pid = spawn(EsioCi.Builder, :build, [])
+            send pid, {self, conn, build_id}
+            conn
+            |> send_resp(200, "Build created with id #{build_id}")
 
-  # deprecated
-  post "/api/v1/bld" do
-    # Insert build to database
-    build = %EsioCi.Build{state: "CREATED"}
-    created_build = EsioCi.Repo.insert!(build)
-    build_id = created_build.id
-
-    pid = spawn(EsioCi.Builder, :build, [])
-    send pid, {self, conn, build_id}
-
-    conn
-    |> send_resp(200, "Build created")
+       _ -> conn
+            |> send_resp(503, "Something get wrong...")
+    end
   end
 
   # 404
   match _ do
     conn
     |> send_resp(404, "404 Nothing here")
+  end
+
+  defp add_build_to_db(project_id) do
+    build = %EsioCi.Build{state: "CREATED", project_id: project_id}
+    created_build = EsioCi.Repo.insert!(build)
+    created_build.id
   end
 end
--- test/esioci_router_test.exs
+++ test/esioci_router_test.exs
@@ -14,6 +14,16 @@ defmodule Esioci.Router.Test do
     assert conn.resp_body == "EsioCi app alpha"
   end
 
+  test "can't create build for nonexisting project" do
+    conn = conn(:post, "/api/v1/esiononexistingproject/bld")
+
+    conn = EsioCi.Router.call(conn, @opts)
+
+    assert conn.state == :sent
+    assert conn.status == 404
+    assert conn.resp_body == "404: Project esiononexistingproject not found."
+  end
+
   test "returns 404" do
     conn = conn(:get, "/esioesioesio")
 
