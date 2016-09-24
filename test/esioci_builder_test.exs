d28589f6e187c824c5f81d0840b7b81f0ceaca75
Grzegorz Eliszewski <grzegorz@eliszewski.pl>
2016-09-24 17:30:13 +0200

    Refactoring EsioCi.Builder module, move build outside of parsing yaml for easiest Unit Tests. Improve UT coverage and stability.

--- test/esioci_builder_test.exs
+++ test/esioci_builder_test.exs
@@ -25,17 +25,17 @@ defmodule EsioCi.Builder.Tests do
 
   test 'test parse yaml' do
     expect(EsioCi.Common, :run, fn(cmd, dir) -> :ok end)
-    assert EsioCi.Builder.parse_yaml({:ok, Path.absname("test/test_yaml_ok")}) == :ok
+    assert EsioCi.Builder.parse_yaml({:ok, Path.absname("test/test_yaml_ok")}) == {:ok, ['mix deps.get && mix.compile'], nil}
   end
 
   test 'test parse yaml with more than one exec' do
     expect(EsioCi.Common, :run, fn(cmd, dir) -> :ok end)
-    assert EsioCi.Builder.parse_yaml({:ok, Path.absname("test/test_yaml_two_execs")}) == :ok
+    assert EsioCi.Builder.parse_yaml({:ok, Path.absname("test/test_yaml_two_execs")}) == {:ok, ['mix deps.get', 'mix.compile'], nil}
   end
 
   test 'test parse yaml with artifacts' do
     expect(EsioCi.Common, :run, fn(cmd, dir) -> :ok end)
-    assert EsioCi.Builder.parse_yaml({:ok, Path.absname("test/test_yaml_artifacts")}) == :ok
+    assert EsioCi.Builder.parse_yaml({:ok, Path.absname("test/test_yaml_artifacts")}) == {:ok, ['mix deps.get && mix.compile'], "test_file"}
   end
 
   test 'test parse broken yaml' do
@@ -43,4 +43,25 @@ defmodule EsioCi.Builder.Tests do
     assert_raise MatchError, fn -> EsioCi.Builder.parse_yaml({:ok, Path.absname("test/test_yaml_broken")}) end
   end
   
+  test 'test parse nonexisting yaml' do
+    expect(EsioCi.Common, :run, fn(cmd, dir) -> :ok end)
+    assert EsioCi.Builder.parse_yaml({:ok, Path.absname("test/no_yaml")}) == {:error, [], []}
+  end
+end
+
+defmodule EsioCi.Builder.Tests.NoMeck do
+  # tests EsioCi.Builder class without meck
+  use ExUnit.Case
+
+  @test_dir "/tmp/test_esioci_builder_test_nomeck"
+
+  setup do
+    File.rm_rf @test_dir
+    on_exit fn -> File.rm_rf @test_dir end
+    :ok
+  end
+
+  test 'test clone repository' do
+    EsioCi.Builder.clone({:ok, "git://github.com/esioci/esioci", "esioci/esioci", "fake", @test_dir})
+  end
 end
