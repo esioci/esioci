ef1fbde3f1ad4bd3b522d3ace73c13269152c79c
Grzegorz Eliszewski <grzegorz@eliszewski.pl>
2016-11-09 18:04:06 +0100

    Remove old commented, code

--- lib/esioci_db.ex
+++ lib/esioci_db.ex
@@ -3,11 +3,6 @@ defmodule EsioCi.Db do
   import Ecto.Query, only: [from: 2]
 
   def get_build_with_id(p_id, b_id) do
-    # case b_id do
-    #   "last" -> build = get_last_build_from_project(p_id) |> EsioCi.Repo.all |> EsioCi.Repo.preload(:project)
-    #    b_id  -> build = get_build(p_id, b_id) |> EsioCi.Repo.all |> EsioCi.Repo.preload(:project)
-    #    _ -> Logger.error "DUPA"
-    # end
     cond do
       b_id == "last"                -> p_id |> get_last_build_from_project |> EsioCi.Repo.all |> EsioCi.Repo.preload(:project)
       b_id == "all"                 -> p_id |> get_builds |> EsioCi.Repo.all |> EsioCi.Repo.preload(:project)
