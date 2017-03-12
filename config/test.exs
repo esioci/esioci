0f38651482dc773121de1d371aa1b9d79f9b17b7
Grzegorz Eliszewski <grzegorz@eliszewski.pl>
2017-03-12 15:37:51 +0100

    Add config for testing.

--- config/test.exs
+++ config/test.exs
@@ -1,1 +1,9 @@
 use Mix.Config
+
+config :esioci, EsioCi.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  pool: Ecto.Adapters.SQL.Sandbox,
+  database: "esioci_test",
+  username: "postgres",
+  password: "",
+  hostname: "localhost"
