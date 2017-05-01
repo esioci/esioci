9b2deb26a5cb2304bb8beba0a46f6b86edeb8dc7
Grzegorz Eliszewski <grzegorz@eliszewski.pl>
2017-05-01 07:26:33 +0200

    Add initial version of timeout poller.

---
+++ lib/esioci_timeout_poller.ex
@@ -0,0 +1,23 @@
+defmodule EsioCi.TimeoutPoller do
+  @moduledoc """
+  This is a timeout poller module, runs every 5 minutes
+  and stop all timeouted builds
+  """
+  use GenServer
+  require Logger
+  
+  def start_link do
+    GenServer.start_link(EsioCi.TimeoutPoller, %{}, [name: EsioCi.TimeoutPoller])
+  end
+
+  def init(state) do
+    Process.send_after(self, :poll, 300 * 1000)
+    {:ok, state}
+  end
+
+  def handle_info(:poll, state) do
+    Logger.debug "Run timeout poller"
+    Process.send_after(self, :poll, 300 * 1000)
+    {:noreply, state}
+  end
+end
