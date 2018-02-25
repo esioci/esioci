6e700db97820596833a23e679f881b283c68d31a
Grzegorz Eliszewski <grzegorz@eliszewski.pl>
2018-02-25 08:34:00 +0100

    Migrate project to elixir 1.6.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EsioCi.Mixfile do
   def project do
     [app:               :esioci,
      version:           "0.5.0",
-     elixir:            "~> 1.4",
+     elixir:            "~> 1.6.1",
      build_embedded:    Mix.env == :prod,
      start_permanent:   Mix.env == :prod,
      deps:              deps(),
